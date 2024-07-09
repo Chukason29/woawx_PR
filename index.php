6bad6b51ee90ad3063672500a702b3881020490e
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-09 15:29:23 +0100

    Initial commit of pr.waowx.commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="WAOWX pr public relations Abuja branding marketing ">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/6c1ada6317.js" crossorigin="anonymous"></script>
+    <title>PR | WAOWX Innovations</title>
+    <link rel="icon" href="./img/waowx-favicon.png" type="image/png">
+</head>
+<body>
+    
+</body>
+</html>
