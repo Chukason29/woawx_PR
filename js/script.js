a5d699912e5c6503e49bcd65ff4f3747d682e134
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-12 19:39:19 +0100

    added animation

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//toggling through the menu system
+openMenu.addEventListener("click", toggleMenu);
+closeMenu.addEventListener("click", toggleMenu);
+$(document).ready(function() {
+    $(window).scroll(function() {
+        $('.animated-element').each(function() {
+            var position = $(this).offset().top + 95;
+            var scroll = $(window).scrollTop();
+
+            if (position < scroll + $(window).height()) {
+				//$(this).addClass('animate__animated');
+				$(this).addClass('visible');
+            }
+        });
+        $('.header')
+    });
+});
