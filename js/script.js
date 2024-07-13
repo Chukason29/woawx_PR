f7cf779e357d83ad85af66b10d57205f563c77aa
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-13 15:29:00 +0100

    Can now open menu

--- js/script.js
+++ js/script.js
@@ -12,3 +12,22 @@ $(document).ready(function() {
         $('.header')
     });
 });
+
+const menuIcon = document.getElementById("menu-icon");
+const menuNav = document.getElementById("menu-nav");
+const closeMenu = document.getElementById("close-menu");
+const openMenu = document.getElementById("open-menu");
+
+function toggleMenu() {
+    if (!menuNav.classList.contains("show-menu")) {
+        menuNav.classList.add("show-menu");
+        menuNav.classList.remove("hide-menu");
+    } else {
+        menuNav.classList.add("hide-menu");
+        menuNav.classList.remove("show-menu");
+    }
+}
+
+//toggling through the menu system
+openMenu.addEventListener("click", toggleMenu);
+closeMenu.addEventListener("click", toggleMenu);
